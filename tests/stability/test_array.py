7b02ba99828ae49964e024f7bf6bd1db3c98544f
Hendrik Makait <hendrik@coiled.io>
2023-02-21 19:17:07 +0100

    Adjust A/B test

--- tests/stability/test_array.py
+++ tests/stability/test_array.py
@@ -12,7 +12,7 @@ def test_rechunk_in_memory(small_client):
     x.rechunk((50000, 20)).rechunk((20, 50000)).sum().compute()
 
 
-@pytest.mark.skip(reason="this runs forever")
+# @pytest.mark.skip(reason="this runs forever")
 def test_rechunk_out_of_memory(small_client):
     x = da.random.random((100000, 100000))
     x.rechunk((50000, 20)).rechunk((20, 50000)).sum().compute()
