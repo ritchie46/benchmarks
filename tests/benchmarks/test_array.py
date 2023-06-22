2c00328499e0d28fb5161568b01651352d992fde
Hendrik Makait <hendrik@coiled.io>
2023-06-22 15:03:16 +0200

    Configure AB

--- tests/benchmarks/test_array.py
+++ tests/benchmarks/test_array.py
@@ -245,10 +245,11 @@ def test_rechunk_striping(small_client, configure_rechunking):
 
 
 @run_up_to_nthreads("small_cluster", 50, reason="fixed dataset")
-def test_rechunk_swap_axes(small_client, configure_rechunking):
+@pytest.mark.parametrize("factor", [0.25, 0.5, 1, 2, 4])
+def test_rechunk_swap_axes(small_client, configure_rechunking, factor):
     rng = da.random.default_rng()
-    x = rng.random((100_000, 100_000), chunks=(100_000, 100))
-    x.rechunk((100, 100_000)).sum().compute()  # ~76 MiB chunks
+    x = rng.random((100_000 * factor, 100_000 * factor), chunks=(100_000 / factor, 100 * factor))
+    x.rechunk((100 * factor, 100_000 / factor)).sum().compute()  # ~76 MiB chunks
 
 
 @run_up_to_nthreads("small_cluster", 50, reason="fixed dataset")
