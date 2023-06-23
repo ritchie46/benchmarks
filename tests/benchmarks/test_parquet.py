674724903c18ac13163a88fc226b16c45d61adc4
crusaderky <crusaderky@gmail.com>
2023-06-23 15:12:58 +0100

    refactor

--- tests/benchmarks/test_parquet.py
+++ tests/benchmarks/test_parquet.py
@@ -106,7 +106,6 @@ def test_write_wide_data(parquet_client, s3_url):
         "pandas",
         "pandas+boto3",
         "dask",
-        "(cooldown)",
     ],
 )
 def test_download_throughput(parquet_client, kind):
@@ -149,9 +148,10 @@ def test_download_throughput(parquet_client, kind):
         fut = parquet_client.submit(pandas_boto3_load, path)
     elif kind == "dask":
         fut = dd.read_parquet(path, engine="pyarrow")
-    elif kind == "(cooldown)":
-        import time
-        time.sleep(600)
-        pytest.skip("just a sleep")
 
     wait(fut, parquet_client, timeout=60)
+
+
+def test_zzz_cooldown():
+    import time
+    time.sleep(600)
